9245d4a729590f1213574260a2f62ce7a20e7ebd
johnneon <neon.1598@mail.ru>
2020-09-30 11:55:33 +0600

    feat: add min() function and check for arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-    return 0;
+    if(arguments.length == 0) return 0;
+    if(array.length == 0) return 0;
+    let sortArr = array.sort((a,b) => a - b)
+    return sortArr[0];
 }
 
 exports.max = function max (array) {
+    if(arguments.length == 0) return 0;
+    if(array.length == 0) return 0;
     return 0;
 }
 
 exports.avg = function avg (array) {
+    if(arguments.length == 0) return 0;
+    if(array.length == 0) return 0;
     return 0;
 }
