c4436b80b2eecf4a823c03652715b4b1ec984492
johnneon <neon.1598@mail.ru>
2020-09-30 11:56:46 +0600

    feat: add avg() function

--- src/index.js
+++ src/index.js
@@ -16,5 +16,9 @@ exports.max = function max (array) {
 exports.avg = function avg (array) {
     if(arguments.length == 0) return 0;
     if(array.length == 0) return 0;
-    return 0;
+    let result = 0;
+    for(let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result / array.length;
 }
